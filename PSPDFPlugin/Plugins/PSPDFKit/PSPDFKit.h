08b261d62621dfc166e56938987fd1efc739835c
Peter Steinberger <me@petersteinberger.com>
2013-06-06 23:23:06 +0200

    Add License

--- PSPDFPlugin/Plugins/PSPDFKit/PSPDFKit.h
+++ PSPDFPlugin/Plugins/PSPDFKit/PSPDFKit.h
@@ -1,9 +1,13 @@
 //
 //  PSPDFKit.h
-//  PSPDFPlugin
+//  PSPDFPlugin for Apache Cordova
 //
-//  Created by Nick Lockwood on 04/06/2013.
+//  Copyright 2013 PSPDFKit GmbH. All rights reserved.
 //
+//  THIS SOURCE CODE AND ANY ACCOMPANYING DOCUMENTATION ARE PROTECTED BY AUSTRIAN COPYRIGHT LAW
+//  AND MAY NOT BE RESOLD OR REDISTRIBUTED. USAGE IS BOUND TO THE PSPDFKIT LICENSE AGREEMENT.
+//  UNAUTHORIZED REPRODUCTION OR DISTRIBUTION IS SUBJECT TO CIVIL AND CRIMINAL PENALTIES.
+//  This notice may not be removed from this file.
 //
 
 #import <Foundation/Foundation.h>
