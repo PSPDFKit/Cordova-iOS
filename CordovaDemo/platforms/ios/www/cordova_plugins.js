a493f202fc569d9e33033c9c2313b985aa2311e1
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-19 14:06:14 +0100

    Add new demo

---
+++ CordovaDemo/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.pspdfkit.cordovaplugin/PSPDFKitPlugin/pspdfkit.js",
+        "id": "com.pspdfkit.cordovaplugin.PSPDFKitPlugin",
+        "clobbers": [
+            "PSPDFKitPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.pspdfkit.cordovaplugin": "1.1.0"
+}
+// BOTTOM OF METADATA
+});
